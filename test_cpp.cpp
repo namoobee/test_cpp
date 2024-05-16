73437f1793126dabb6ccd90a405850012f5d310b
namoobee <https://github.com/namoobee>
2024-05-16 20:32:32 +0900

    polymorphism

--- test_cpp.cpp
+++ test_cpp.cpp
@@ -1,49 +1,65 @@
 ﻿#include <iostream>
+#include <string>
+
 using namespace std;
 
-class Shape
-{
-protected:
-	virtual void draw() = 0;
-};
-
-class Circle : public Shape
+class Snack
 {
 public:
-	void draw() override
+	int Price; // 가격
+	string Name; // 상품이름
+	string Company; // 제조회사
+
+	Snack() : Price(0), Name(""), Company("") {}
+
+	Snack(int price, string name, string company)
 	{
-		cout << "Circle" << endl;
+		this->Price = price;
+		this->Name = name;
+		this->Company = company;
 	}
 };
 
-class Rectangle : public  Shape
+
+class Candy : public Snack
 {
 public:
-	void draw() override
+	string Taste; // 맛
+
+	Candy(string taste, int price, string name, string company)
 	{
-		cout << "Rectangle" << endl;
+		this->Taste = taste;
+		this->Price = price;
+		this->Name = name;
+		this->Company = company;
 	}
 };
 
-class Triangle : public  Shape
+class Chocolate : public Snack
 {
 public:
-	void draw() override
+	string Shape; // 모양
+
+	Chocolate(string shape, int price, string name, string company)
 	{
-		cout << "Triangle" << endl;
+		this->Shape = shape;
+		this->Price = price;
+		this->Name = name;
+		this->Company = company;
 	}
 };
 
 int main()
 {
-	Circle Cir;
-	Cir.draw();
+	Snack* SnackBasket[4];
+	SnackBasket[0] = new Candy("salty", 1500, "아이셔", "오리온");
+	SnackBasket[1] = new Candy("sweet", 1000, "땅콩캬라멜", "크라운");
+	SnackBasket[2] = new Chocolate("rectangle", 2100, "가나", "롯데제과");
+	SnackBasket[3] = new Chocolate("triangle", 2500, "토블론", "몬델리즈");
 
-	Rectangle Rect;
-	Rect.draw();
-
-	Triangle Tri;
-	Tri.draw();
-
+	for (int i = 0; i < 4; i++)
+	{
+		cout << SnackBasket[i]->Name << endl;
+	}
 	return 0;
 }
