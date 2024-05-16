8ad2220c03aee39dd70542d0544aba008e28a6f7
namoobee <https://github.com/namoobee>
2024-05-16 17:44:46 +0900

    virtual_class

--- test_cpp.cpp
+++ test_cpp.cpp
@@ -3,67 +3,47 @@ using namespace std;
 
 class Shape
 {
-public:
-	int NumOfSides; // 변의 개수
-	int width; // 밑변의 길이
-
-	Shape(int num_of_sides, int w) : NumOfSides(num_of_sides), width(w) {} // 생성자
-
-	void printInfo() // 변의 개수와 밑변의 길이를 출력
-	{
-		cout << "변의 개수: " << NumOfSides << endl << "밑변의 길이: " << width << endl;
-	};
+protected:
+	virtual void draw() = 0;
 };
 
-class Rectangle : public Shape
+class Circle : protected Shape
 {
 public:
-	int Rect_height; // 세로 길이
-	int Rect_area; // 넓이
-
-	Rectangle(int num_of_sides, int w, int h): Shape(num_of_sides, w), Rect_height(h), Rect_area(w * h) {} // 생성자
-
-	void area() // 넓이를 구하고 출력
+	void draw() override
 	{
-		cout << "사각형의 넓이: " << Rect_area << endl;
+		cout << "Circle" << endl;
 	}
-
-	void printInfo(int w, int h) // 변의 개수와 밑변의 길이를 출력
-	{
-		cout << "사각형의 넓이는 " << w * h << "입니다." << endl;
-	};
 };
 
-class Triangle : public Shape
+class Rectangle : protected Shape
 {
-	int Tri_height; //높이 길이
-	int Tri_area; // 넓이
-
 public:
-	Triangle(int num_of_sides, int w, int h) : Shape(num_of_sides, w), Tri_height(h), Tri_area(w* h / 2) {} //생성자
-
-	void area()
+	void draw() override
 	{
-		cout << "삼각형의 넓이: " << Tri_area << endl;
+		cout << "Rectangle" << endl;
 	}
+};
 
-	void printInfo(int w, int h) // 변의 개수와 밑변의 길이를 출력
+class Triangle : protected Shape
+{
+public:
+	void draw() override
 	{
-		cout << "삼각형의 넓이는 " << w * h / 2 << "입니다." << endl;
-	};
+		cout << "Triangle" << endl;
+	}
 };
 
 int main()
 {
-	Rectangle rect(4, 4, 3); //변의 개수, 밑변의 길이, 높이
-	Triangle tri(3, 4, 3); // 변의 개수, 밑변의 길이, 높이
-	// rect.area();
-	// tri.area();
+	Circle Cir;
+	Cir.draw();
 
-	rect.Shape::printInfo();
-	rect.printInfo(5, 2);
+	Rectangle Rect;
+	Rect.draw();
 
-	tri.Shape::printInfo();
-	tri.printInfo(2, 3);
+	Triangle Tri;
+	Tri.draw();
+
 	return 0;
 }
