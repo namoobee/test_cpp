9d2f64b7c63dcbc6215d00a2d05f83bab34789d2
namoobee <https://github.com/namoobee>
2024-05-20 16:19:41 +0900

    polymorphism_V2

--- test_cpp.cpp
+++ test_cpp.cpp
@@ -18,6 +18,7 @@ public:
 		this->Name = name;
 		this->Company = company;
 	}
+	virtual void PrintCompany() = 0; 
 };
 
 
@@ -33,6 +34,10 @@ public:
 		this->Name = name;
 		this->Company = company;
 	}
+	void PrintCompany() override
+	{
+		cout << "제조사: " << this->Company << endl;
+	}
 };
 
 class Chocolate : public Snack
@@ -47,19 +52,30 @@ public:
 		this->Name = name;
 		this->Company = company;
 	}
+	void PrintCompany() override
+	{
+		cout << "제조사: " << this->Company << endl;
+	}
 };
 
 int main()
 {
-	Snack* SnackBasket[4];
+	Snack* SnackBasket[4]; 
 	SnackBasket[0] = new Candy("salty", 1500, "아이셔", "오리온");
-	SnackBasket[1] = new Candy("sweet", 1000, "땅콩캬라멜", "크라운");
-	SnackBasket[2] = new Chocolate("rectangle", 2100, "가나", "롯데제과");
-	SnackBasket[3] = new Chocolate("triangle", 2500, "토블론", "몬델리즈");
+	SnackBasket[1] = new Candy("sweet", 1000, "땅콩캬라멜", "크라운"); 
+	SnackBasket[2] = new Chocolate("rectangle", 2100, "가나", "롯데제과"); 
+	SnackBasket[3] = new Chocolate("triangle", 2500, "토블론", "몬델리즈"); 
 
 	for (int i = 0; i < 4; i++)
 	{
-		cout << SnackBasket[i]->Name << endl;
+		cout << SnackBasket[i]->Name << ", "; 
+		SnackBasket[i]->PrintCompany();
+	}
+	// 동적 할당된 메모리를 해제
+	for (int i = 0; i < 4; i++)
+	{
+		delete SnackBasket[i];
 	}
 	return 0;
 }
+// candy, chocolate의 생성자에서 각각 제조사를 입력 받는다는 것은 main함수의 
